2c5b7b1c5dfb778a6b90530499123bfefd50b451
Lindsay Spencer <lindsay.m.spencer@gmail.com>
2019-02-25 17:47:43 -0800

    Lesson 5. Added pdfs.

---
+++ Lesson 5/Books.py
@@ -0,0 +1,21 @@
+import json
+
+book1 = dict(title = "Learning Python", author = "Mark Lutz", PubDate = 2013 )
+book2 = dict(title = "As I Lay Dying", author = "William Faulkner", PubDate = 930 )
+
+tempTitle = input("Enter Title: ")
+tempAuthor = input("Enter author: ")
+tempPubDate = int(input("Enter Publication Year: "))
+
+book3 = dict(title = tempTitle, author = tempAuthor, PubDate = tempPubDate )
+
+books = [book1, book2, book3]
+
+json.dump(books, fp=open('myFile.txt','w'), indent=4)
+
+newBook = json.load(open('myFile.txt'))
+print(newBook)
+print(len(newBook))
+
+for book in newBook:
+    print(book["title"])
