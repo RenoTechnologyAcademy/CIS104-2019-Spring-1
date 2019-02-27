141f406c93c1a51d2862a7b0b421f371137421f9
Lindsay Spencer <lindsay.m.spencer@gmail.com>
2019-02-26 19:44:59 -0800

    changes

--- Lesson 5/Books.py
+++ Lesson 5/Books.py
@@ -7,9 +7,10 @@ tempTitle = input("Enter Title: ")
 tempAuthor = input("Enter author: ")
 tempPubDate = int(input("Enter Publication Year: "))
 
-book3 = dict(title = tempTitle, author = tempAuthor, PubDate = tempPubDate )
+books = [book1, book2]
 
-books = [book1, book2, book3]
+book3 = dict(title = tempTitle, author = tempAuthor, PubDate = tempPubDate )
+books.append(book3)
 
 json.dump(books, fp=open('myFile.txt','w'), indent=4)
 
