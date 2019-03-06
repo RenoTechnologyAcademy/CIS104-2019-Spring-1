062d3498d332e3caffc28714f6c2dc3413342eaa
Lindsay Spencer <lindsay.m.spencer@gmail.com>
2019-03-05 18:13:28 -0800

    Lesson 6

--- foo.py
+++ foo.py
@@ -1,7 +1,3 @@
-pages = int(input("pages?"))
-if(pages < 100):
-    print("This book is a short book.")
-elif(pages < 300):
-    print("This book is an average book.")
-else:
-    print("This book is a long book.")   
+mySet = set(("red", "blue", "green"))
+mySet.pop()
+print(mySet)
